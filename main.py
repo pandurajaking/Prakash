03202e0079c6e7cdf2432c5f7eb97134f3646cd7
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-20 12:33:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -885,7 +885,7 @@ async def account_login(bot: Client, m: Message):
                     'x-access-token':
                     'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0',
                     'user-agent': 'Mobile-Android',
-                    'app-version': '1.4.37.1',
+                    'app-version': '1.4.71.1',
                     'api-version': '18',
                     'device-id': '5d0d17ac8b3c9f51',
                     'device-details':
@@ -905,7 +905,7 @@ async def account_login(bot: Client, m: Message):
 
                 headers1 = {
                     'User-Agent':
-                    'ExoPlayerDemo/1.4.37.1 (Linux;Android 11) ExoPlayerLib/2.14.1',
+                    'ExoPlayerDemo/1.4.71.1 (Linux;Android 11) ExoPlayerLib/2.14.1',
                     'Accept-Encoding': 'gzip',
                     'Host': 'cdn.jwplayer.com',
                     'Connection': 'Keep-Alive',
