282471c4172e972abb5649512c5548637d7cc048
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-24 21:11:13 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 20717621
 api_hash = "a68af155e7f0c79b1baab1f185f4181d"
-bot_token = "6671981603:AAFICZJcnCFLsK3vHdusm6NWmt4SzqPjIOE"
+bot_token = "6524877415:AAGVC_Hxv4mVXhwURjguYsDcB2iV5NGlkqM"
 
