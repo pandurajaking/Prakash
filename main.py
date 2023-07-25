5a91a0589a9224759ee8ae118aab8727a8a38e80
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-25 10:40:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -946,8 +946,8 @@ async def account_login(bot: Client, m: Message):
                 else:
                     await m.reply_video(filename,
                                         supports_streaming=True,
-                                        height=360
-                                        width=640
+                                        height=360,480,720,
+                                        width=640,854,1280,
                                         caption=cc,
                                         duration=dur,
                                         thumb=thumbnail,
