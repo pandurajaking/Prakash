1313ba504f5cfbc9b9552c2851ce139c57f43f7c
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-26 18:47:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -916,13 +916,7 @@ async def account_login(bot: Client, m: Message):
             prog = await m.reply_text(Show)
             cc = f'**Title »** {name1}.mkv\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- @Prakash_Baraiya'
             if "pdf" in url:
-                response = requests.get(url1)
-                if response.status_code == 200:
-                    with open(f"{name}.pdf", "wb") as f:
-                        f.write(response.content)
-                else:
-                    await m.reply_text(f"Failed to download PDF: {name}")
-                    continue  
+                cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
             else:
                 cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --remux-video mkv --format "bestvideo[height<={raw_text2}]+bestaudio/best[height<={raw_text2}]" "{url1}"'
 
