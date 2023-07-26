484599721a28609ffeb253b8dfa9518fca30eca4
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-26 17:59:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ import aiohttp
 import asyncio
 import aiofiles
 from pyrogram.types import User, Message
-import progressor
-from progressor import progress_for_pyrogram
+#import progressor
+#from progressor import progress_for_pyrogram
 import sys
 import re
 import os
