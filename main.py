67fee61740b375e2cd68900df5c629158bb42be6
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-27 14:13:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -920,10 +920,14 @@ async def account_login(bot: Client, m: Message):
             Show = f"**Downloading:-**\n\n**Name :-** `{name}`\n\n**Url :-** `{url1}`"
             prog = await m.reply_text(Show)
             cc = f'**Title »** {name1}.mkv\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- @Prakash_Baraiya'
-            if "pdf" in url:
-                cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
+            if "pdf" in url1:
+                name = f'{str(count).zfill(3)}) {name1}.pdf'
+                if not os.path.isfile(f"{name}.pdf"):
+                    cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
             else:
-                cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --remux-video mkv --format "bestvideo[height<={raw_text2}]+bestaudio/best[height<={raw_text2}]" "{url1}"'
+                name = f'{str(count).zfill(3)}) {name1}.mkv'
+                if not os.path.isfile(f"{name}.mkv"):
+                    cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --remux-video mkv --format "bestvideo[height<={raw_text2}]+bestaudio/best[height<={raw_text2}]" "{url1}"'
 
             try:
                 download_cmd = f"{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
