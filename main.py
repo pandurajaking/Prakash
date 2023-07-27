192dad95f09e87e879b38d0eae40da31d115f1f5
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-27 15:06:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -927,7 +927,7 @@ async def account_login(bot: Client, m: Message):
                 name = f'{str(count).zfill(3)}) {name1}.pdf'
                 if not os.path.isfile(f"{name}.pdf"):
                     cmd = f'yt-dlp -o "{name}.pdf" "{url1}"'
-            else:
+             else:
                 name = f'{str(count).zfill(3)}) {name1}.mkv'
                 if not os.path.isfile(f"{name}.mkv"):
                     cmd = f'yt-dlp -o "{name}.mp4" --no-keep-video --remux-video mkv --format "bestvideo[height<={raw_text2}]+bestaudio/best[height<={raw_text2}]" "{url1}"'
