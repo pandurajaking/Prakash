e2bc9ee44437f7600dc13d88adfeed4ad7e81bd6
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-07-28 20:09:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -892,8 +892,8 @@ async def account_login(bot: Client, m: Message):
                     'x-access-token':
                     'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0',
                     'user-agent': 'Mobile-Android',
-                    'app-version': '1.4.39.5',
-                    'api-version': '20',
+                    'app-version': '1.4.73.2',
+                    'api-version': '35',
                     'device-id': '5d0d17ac8b3c9f51',
                     'device-details':
                     '2848b866799971ca_2848b8667a33216c_SDK-30',
