078a6a6876e98f682fc4f0cd19e9d807215126d6
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-09-01 14:46:05 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,3 +1,3 @@
 api_id = "11657097"
 api_hash = "7198384c0cc8cb877e4731d14e2dd7b8"
-bot_token = "6486166910:AAF710gLtMeIuxo_IwQSjUoXAQkm9tjLgDU"
+bot_token = "6332684382:AAEldTQynP4KQdSSTyteflVFe4s73sFzAqI"
