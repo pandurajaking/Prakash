1a27081f01085d7c1d1cf9450524344fcf80871b
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-09-02 08:48:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -411,8 +411,8 @@ async def account_login(bot: Client, m: Message):
             try:
                 Show = f"**Downloading:-**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-** `{url}`"
                 prog = await m.reply_text(Show)
-                cc = f"**Name »** {name1} {res}.mp4\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- ◤SPIDER★MAN 🕸️ 🕷️"
-                cc1 = f"**Name »** ** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :-                                  ◤SPIDER★MAN 🕸️ 🕷️"
+                cc = f"**Name »** {name1} {res}.mp4\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- ▄︻┻┳═一STUDEŊT乡✓ 
+                cc1 = f"**Name »** ** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :-                                            ▄︻┻┳═一STUDEŊT乡✓  "
                 #                         await prog.delete (True)
                 #                 if cmd == "pdf" or "drive" in url:
                 #                     try:
@@ -444,7 +444,7 @@ async def account_login(bot: Client, m: Message):
                         await m.reply_document(
                             ka,
                             caption=
-                            f"**Name »** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :-                                     ◤SPIDER★MAN 🕸️ 🕷️"
+                            f"**Name »** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :-                                       ▄︻┻┳═一STUDEŊT乡✓"
                         )
                         count += 1
                         # time.sleep(1)
@@ -925,10 +925,10 @@ async def account_login(bot: Client, m: Message):
             prog = await m.reply_text(Show)
             if "pdf" in url:
                 filename = f"{name}.pdf"
-                cc = f'**Title »** {name1}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :-                                       ◤SPIDER★MAN 🕸️ 🕷️'
+                cc = f'**Title »** {name1}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :-                                                  ▄︻┻┳═一STUDEŊT乡✓  '
             else:
                 filename = f"{name}.mp4" if os.path.isfile(f"{name}.mp4") else f"{name}.mkv"
-                cc = f'**Title »** {name1}.mp4\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- ◤SPIDER★MAN 🕸️ 🕷️'
+                cc = f'**Title »** {name1}.mp4\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- ▄︻┻┳═一STUDEŊT乡✓'
             
             
             if "pdf" in url:
