f1e12fd693c881f730ac2970903f9b5755e696fd
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-09-05 11:20:24 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -1,23 +1,5 @@
-import time
-import math
-import os
-from Easy_F import hrb,hrt
-from pyrogram.errors import FloodWait
-
-class Timer:
-    def __init__(self, time_between=5):
-        self.start_time = time.time()
-        self.time_between = time_between
-
-    def can_send(self):
-        if time.time() > (self.start_time + self.time_between):
-            self.start_time = time.time()
-            return True
-        return False
-
-timer = Timer()
-async def progress_bar(current,total,reply,start):
-      if timer.can_send():
+async def progress_bar(current, total, reply, start):
+    if timer.can_send():
         now = time.time()
         diff = now - start
         if diff < 1:
@@ -26,11 +8,19 @@ async def progress_bar(current,total,reply,start):
             perc = f"{current * 100 / total:.1f}%"
             elapsed_time = round(diff)
             speed = current / elapsed_time
-            sp=str(hrb(speed))+"ps"
-            tot=hrb(total)
-            cur=hrb(current)
+            sp = str(hrb(speed)) + "ps"
+            tot = hrb(total)
+            cur = hrb(current)
+
+            # Calculate ETA
+            remaining = total - current
+            eta_seconds = round(remaining / speed)
+            eta = hrt(eta_seconds)
+
             try:
-               await reply.edit(f'┌ <b>Progress</b> 📈 -【 {perc} 】\n├ <b>Speed</b> 🧲 -【 {sp} 】\n└ <b>Size</b> 📂 -【 {cur} / {tot} 】')
-               
+                await reply.edit(f'┌ <b>Progress</b> 📈 -【 {perc} 】\n'
+                                f'├ <b>Speed</b> 🧲 -【 {sp} 】\n'
+                                f'├ <b>Size</b> 📂 -【 {cur} / {tot} 】\n'
+                                f'└ <b>ETA</b> ⏳ -【 {eta} 】')
             except FloodWait as e:
                 time.sleep(e.x)
