a525de14e36ba34fb8a56dc858ae898925c27c23
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-09-09 23:32:42 +0530

    Update Easy_F.py

--- Easy_F.py
+++ Easy_F.py
@@ -1,45 +1,35 @@
 from datetime import datetime,timedelta
 
-def hrb(value, digits= 2, delim= "", postfix=""):
-    """Return a human-readable file size.
-    """
+def hrb(value: int, digits: int = 2, delim: str = "", postfix: str = "") -> str:
+    """Return a human-readable file size."""
     if value is None:
         return None
+
+    units = ["B", "KiB", "MiB", "GiB", "TiB"]
     chosen_unit = "B"
-    for unit in ("KiB", "MiB", "GiB", "TiB"):
-        if value > 1000:
-            value /= 1024
-            chosen_unit = unit
-        else:
+    for unit in units:
+        if value < 1024:
             break
-    return f"{value:.{digits}f}" + delim + chosen_unit + postfix
+        value /= 1024
+        chosen_unit = unit
 
-def hrt(seconds, precision = 0):
-    """Return a human-readable time delta as a string.
-    """
+    return f"{value:.{digits}f}{delim}{chosen_unit}{postfix}"
+
+def hrt(seconds: int, precision: int = 0) -> str:
+    """Return a human-readable time delta as a string."""
     pieces = []
-    value = timedelta(seconds=seconds)
-    
 
-    if value.days:
-        pieces.append(f"{value.days}d")
+    if seconds < 1 and precision > 0:
+        return "0s"
 
-    seconds = value.seconds
+    intervals = [("d", 86400), ("h", 3600), ("m", 60), ("s", 1)]
+    for unit, interval in intervals:
+        if seconds >= interval or unit == "s" or not pieces:
+            value = seconds // interval
+            seconds %= interval
+            pieces.append(f"{int(value)}{unit}")
 
-    if seconds >= 3600:
-        hours = int(seconds / 3600)
-        pieces.append(f"{hours}h")
-        seconds -= hours * 3600
-
-    if seconds >= 60:
-        minutes = int(seconds / 60)
-        pieces.append(f"{minutes}m")
-        seconds -= minutes * 60
-
-    if seconds > 0 or not pieces:
-        pieces.append(f"{seconds}s")
-
-    if not precision:
+    if precision == 0:
         return "".join(pieces)
-
-    return "".join(pieces[:precision])
+    else:
+        return "".join(pieces[:precision])
