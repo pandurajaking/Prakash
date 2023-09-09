99dca0dabc1ddfeac0cde4ae8df74d559a71205a
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-09-09 23:36:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pyromod import listen
 from pyrogram.types import Message
 import pyrogram
 import tgcrypto
-from p_bar import update_progress
+from p_bar import progress_bar
 #from details import api_id, api_hash, bot_token
 from subprocess import getstatusoutput
 import helper
