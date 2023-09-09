f24fa00dbc7dfc1bad82f40e1dfe58b63e336cc4
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-09-10 00:56:56 +0530

    Add files via upload

--- p_bar.py
+++ p_bar.py
@@ -1,9 +1,10 @@
+import time
 import math
 import os
-import time
-from Easy_F import hrb, hrt
+from Easy_F import hrb,hrt
 from pyrogram.errors import FloodWait
-from pyrogram.types import Message
+from Easy_F import hrb, hrt
+
 
 class Timer:
     def __init__(self, time_between=5):
@@ -16,43 +17,25 @@ class Timer:
             return True
         return False
 
-async def progress_bar(current, total, reply: Message, start):
-    timer = Timer()
-    retry_count = 0
-    max_retries = 5  # Maximum number of retries
-
-    while retry_count <= max_retries:
-        if timer.can_send():
-            now = time.time()
-            diff = now - start
-            if diff < 1:
-                return
-            else:
-                perc = f"{current * 100 / total:.1f}%"
-                elapsed_time = round(diff)
-                speed = current / elapsed_time
-                sp = str(hrb(speed)) + "ps"
-                tot = hrb(total)
-                cur = hrb(current)
-                remaining = total - current
-                eta_seconds = round(remaining / speed)
-                eta = hrt(eta_seconds)
-                try:
-                    await reply.edit_text(
-                        f'┌ <b>Progress</b> 📈 -【 {perc} 】\n├ <b>Speed</b> 🧲 -【 {sp} 】\n├ <b>Size</b> 📂 -【 {cur} / {tot} 】\n└ <b>ETA</b> ⏳ -【 {eta} 】'
-                    )
-                    return  # Exit the loop if editing is successful
-
-                except FloodWait as e:
-                    time.sleep(e.x)
-                    retry_count += 1
-
-    # If we reach here, it means we exceeded the maximum number of retries
-    print("Maximum retry count reached. Exiting.")
-
-# Example usage:
-# current = 100
-# total = 1000
-# reply_message = await some_function_to_send_message()
-# start_time = time.time()
-# await progress_bar(current, total, reply_message, start_time)
+timer = Timer()
+async def progress_bar(current,total,reply,start):
+      if timer.can_send():
+        now = time.time()
+        diff = now - start
+        if diff < 1:
+            return
+        else:
+            perc = f"{current * 100 / total:.1f}%"
+            elapsed_time = round(diff)
+            speed = current / elapsed_time
+            sp=str(hrb(speed))+"ps"
+            tot=hrb(total)
+            cur=hrb(current)
+            remaining = total - current
+            eta_seconds = round(remaining / speed)
+            eta = hrt(eta_seconds)
+            try:
+               await reply.edit(f'┌ <b>Progress</b> 📈 -【 {perc} 】\n├ <b>Speed</b> 🧲 -【 {sp} 】\n├ <b>Size</b> 📂 -【 {cur} / {tot} 】\n└ <b>ETA</b> ⏳ -【 {eta} 】')
+               
+            except FloodWait as e:
+                time.sleep(e.x)
