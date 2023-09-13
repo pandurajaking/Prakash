1d42da16954c6d9ad32be9b4a46a51591e558007
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-09-13 15:41:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1022,9 +1022,12 @@ try:
                     f"**Downloading failed ❌**\n{str(e)}\n**Name** - {name}\n**Link** - `{url}` & `{url1}`"
                 )
                 continue
+    try:
+    
     except Exception as e:
-        await m.reply_text(e)
-    await m.reply_text("Done")
+         await m.reply_text(str(e))
+     await m.reply_text("Done")
+
 
 
 @bot.on_message(filters.command(["top"]))
