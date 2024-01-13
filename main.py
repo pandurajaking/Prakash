7a7e47ab0b44406546e62a3e81b113d872651b04
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2024-01-13 14:48:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -21,26 +21,25 @@ import aiohttp
 import asyncio
 import aiofiles
 from pyrogram.types import User, Message
-## import progressor
-## from progressor import progress_for_pyrogram
+# import progressor
+# from progressor import progress_for_pyrogram
 import sys
 import re
 import os
 import io
 import logging
-#import pycurl
-import yt_dlp
-import urllib3
-import wget
-
-
-
+from shutil import get_terminal_size
+from io import BytesIO
+from hachoir.parser import createParser
+from hachoir.metadata import extractMetadata
+import pycurl
 
 # bot = Client(
 #     "bot",
 #     api_id=api_id,
 #     api_hash=api_hash,
 #     bot_token=bot_token)
+
 from logging.handlers import RotatingFileHandler
 
 logging.basicConfig(
@@ -63,9 +62,9 @@ bot = Client("bot",
              bot_token=os.environ.get("BOT_TOKEN"),
              api_id=int(os.environ.get("API_ID")),
              api_hash=os.environ.get("API_HASH"))
-auth_users = [5934830127,1085174050,6046547078]
+auth_users = [1085174050,5934830127]
 sudo_users = auth_users
-sudo_groups = [-1001925087804]
+sudo_groups = [-1001663303433]
 
 shell_usage = f"**USAGE:** Executes terminal commands directly via bot.\n\n<pre>/shell pip install requests</pre>"
 def one(user_id):
@@ -153,7 +152,7 @@ async def account_login(bot: Client, m: Message):
         return
     else:
         editable = await m.reply_text(
-            "Hello @Prakash_Baraiya **I am Text Downloader Bot**. I can download videos from **text** file one by one.**\n\nDeveloper** : उससे क्या होगा काम से काम रखो 😂 @Prakash_Baraiya**\nLanguage** : Python**\nFramework** : Pyrogram\n\nSend **TXT** File {Name : Link}")
+            "Hello @Prakash_Baraiya **I am Text Downloader Bot**. I can download videos from **text** file one by one.**\n\nDeveloper** : उससे क्या होगा काम से काम रखो 😂 @Adrenalinators**\nLanguage** : Python**\nFramework** : Pyrogram\n\nSend **TXT** File {Name : Link}")
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
     await input.delete(True)
@@ -165,21 +164,14 @@ async def account_login(bot: Client, m: Message):
             content = f.read()
         content = content.split("\n")
         links = []
-        for line in content:
-            # Use regular expressions to extract URLs starting with http or https
-            matches = re.findall(r'\bhttps?://\S+', line)
-            if matches:
-                url = matches[0]  # Take the first detected URL
-                # Remove the URL part from the line to get the name
-                name = line.replace(url, "").strip()
-                links.append((name, url))
+        for i in content:
+            links.append(i.split(":",1)) 
         os.remove(x)
-# print(len(links))
+        # print(len(links))
     except:
         await m.reply_text("Invalid file input.")
         os.remove(x)
         return
-
 
     editable = await m.reply_text(
         f"Total links found are **{len(links)}**\n\nSend From where you want to download initial is **0**"
@@ -305,8 +297,6 @@ async def account_login(bot: Client, m: Message):
                     ytf = out['852x470']
                 elif '1280x720' in out:
                     ytf = out['1280x720']
-                elif '320x240' in out:
-                    ytf = out['320x240']
                 elif 'unknown' in out:
                     ytf = out["unknown"]
                 else:
@@ -395,7 +385,7 @@ async def account_login(bot: Client, m: Message):
             except Exception:
                 res = "NA"
 
-            # if "youtube" in url:
+            # if "youtu" in url:
             # if ytf == f"'bestvideo[height<={raw_text2}][ext=mp4]+bestaudio[ext=m4a]'" or "acecwply" in url:
             if "acecwply" in url:
                 cmd = f'yt-dlp -o "{name}.%(ext)s" -f "bestvideo[height<={raw_text2}]+bestaudio" --hls-prefer-ffmpeg --no-keep-video --remux-video mp4 --no-warning "{url}"'
@@ -415,8 +405,8 @@ async def account_login(bot: Client, m: Message):
             try:
                 Show = f"**Downloading:-**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-** `{url}`"
                 prog = await m.reply_text(Show)
-                cc = f"**Name »** {name1} {res}.mp4\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- ▄︻┻┳═一STUDEŊT乡✓"
-                cc1 = f"**Name »** ** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :-                                             ▄︻┻┳═一STUDEŊT乡✓"
+                cc = f"**Name »** {name1} {res}.mp4\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}"
+                cc1 = f"**Name »** ** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}"
                 #                         await prog.delete (True)
                 #                 if cmd == "pdf" or "drive" in url:
                 #                     try:
@@ -448,7 +438,7 @@ async def account_login(bot: Client, m: Message):
                         await m.reply_document(
                             ka,
                             caption=
-                            f"**Name »** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :-                                       ▄︻┻┳═一STUDEŊT乡✓"
+                            f"**Name »** {name1} {res}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}"
                         )
                         count += 1
                         # time.sleep(1)
@@ -838,16 +828,10 @@ async def account_login(bot: Client, m: Message):
             content = f.read()
         content = content.split("\n")
         links = []
-        for line in content:
-            # Use regular expressions to extract URLs starting with http or https
-            matches = re.findall(r'\bhttps?://\S+', line)
-            if matches:
-                url = matches[0]  # Take the first detected URL
-                # Remove the URL part from the line to get the name
-                name = line.replace(url, "").strip()
-                links.append((name, url))
+        for i in content:
+            links.append(i.split(":", 1))
         os.remove(x)
-# print(len(links))
+        # print(len(links))
     except:
         await m.reply_text("Invalid file input.")
         os.remove(x)
@@ -903,20 +887,17 @@ async def account_login(bot: Client, m: Message):
                     "@", "").replace("*", "").replace(".", "").strip()
 
             if "videos.classplus" in url:
-
                 headers = {
                     'Host': 'api.classplusapp.com',
                     'x-access-token':
-                    'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6OTI1MzU1NzksIm9yZ0lkIjo0MDMwOTUsInR5cGUiOjEsIm1vYmlsZSI6IjkxNjM1OTE0NjE0NSIsIm5hbWUiOiJQcmFrYXNoIEJhcmFpeWEiLCJlbWFpbCI6InByYWthc2gxNTEwODNAZ21haWwuY29tIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJkZWZhdWx0TGFuZ3VhZ2UiOiJFTiIsImNvdW50cnlDb2RlIjoiSU4iLCJjb3VudHJ5SVNPIjoiOTEiLCJ0aW1lem9uZSI6IkdNVCs1OjMwIiwiaXNEaXkiOmZhbHNlLCJvcmdDb2RlIjoib3hwYmgiLCJmaW5nZXJwcmludElkIjoiYTg0MjNkYjFlZjE5MjI3ZTMyOGFmNGEwMGRlODJlMTEiLCJpYXQiOjE2OTQwNzk1MjYsImV4cCI6MTY5NDY4NDMyNn0.3EatpR80XlzD2q9pImEnvYXieV3SfwckUExG_Y-4NtLk6CSm_dkKPfRKynp-Ed3F',
+                    'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6OTc1OTk5NjMsIm9yZ0lkIjozNTE1MjEsInR5cGUiOjEsIm1vYmlsZSI6IjkxNjM1OTE0NjE0NSIsIm5hbWUiOiJQcmFrYXNoIEJhcmFpeWEiLCJlbWFpbCI6InByYWthc2gxNTEwODNAZ21haWwuY29tIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJkZWZhdWx0TGFuZ3VhZ2UiOiJFTiIsImNvdW50cnlDb2RlIjoiSU4iLCJjb3VudHJ5SVNPIjoiOTEiLCJ0aW1lem9uZSI6IkdNVCs1OjMwIiwiaXNEaXkiOnRydWUsIm9yZ0NvZGUiOiJhdWZpciIsImZpbmdlcnByaW50SWQiOiJjOWIxZDZiZDMxMTI3M2U0OTJiNTcxYTAwMWQ0YTkyNSIsImlhdCI6MTY5MDY5ODczOCwiZXhwIjoxNjkxMzAzNTM4fQ.kDUNPb5EFDp1vmNZ-VcRIBn2vSEz_ssyAxmqbSDLJFD42g7i43aIrWgFbWYfh60L',
                     'user-agent': 'Mobile-Android',
-                    'app-version': '1.4.69',
+                    'app-version': '1.4.76.4',
                     'api-version': '24',
                     'device-id': 'c28d3cb16bbdac01',
                     'device-details':
                     'Xiaomi_Redmi 7_SDK-32',
-                    'accept-encoding': 'gzip, deflate, br',
-                    
-                    
+                    'accept-encoding': 'gzip',
                 }
 
                 params = (('url', f'{url}'), )
@@ -935,10 +916,10 @@ async def account_login(bot: Client, m: Message):
             prog = await m.reply_text(Show)
             if "pdf" in url:
                 filename = f"{name}.pdf"
-                cc = f'**Title »** {name1}.pdf\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :-                                                  ▄︻┻┳═一STUDEŊT乡✓'
+                cc = f'**Title »** {name1}.pdf\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :-                       @Prakash_Baraiya'
             else:
                 filename = f"{name}.mp4" if os.path.isfile(f"{name}.mp4") else f"{name}.mkv"
-                cc = f'**Title »** {name1}.mp4\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- ▄︻┻┳═一STUDEŊT乡✓'
+                cc = f'**Title »** {name1}.mp4\n**Caption »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- @Prakash_Baraiya'
             
             
             if "pdf" in url:
