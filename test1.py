6ac9f6ab47103210f50a3dc7fac61f8cb186ba36
pandurajaking <133520032+pandurajaking@users.noreply.github.com>
2024-05-18 12:39:31 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,15 @@
-import requests as s
+import requests
 
 url = 'https://cpvod.testbook.com/659ba2ba1ffd3734c48d6498/playlist.m3u8'
 
-resp = s.get('https://app.magmail.eu.org/get_keys', json={'link': url})
+headers = {
+    'Host': 'app.magmail.eu.org',
+    'User-Agent': 'Mobile-Android',
+    'Device-ID': 'c28d3cb16bbdac01',
+    'Device-Details': 'Xiaomi_Redmi 7_SDK-32',
+    'Accept-Encoding': 'gzip, deflate, br',
+}
+
+resp = requests.post('https://app.magmail.eu.org/get_keys', json={'link': url}, headers=headers)
+
 print(resp.text)
