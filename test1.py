506104f372ef39834a8b85cca1f119d34eb86a6c
pandurajaking <133520032+pandurajaking@users.noreply.github.com>
2024-05-18 11:52:24 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,30 +2,24 @@ import requests as s
 
 resp = s.post('https://app.magmail.eu.org/get_keys', json={'link':'https://cpvod.testbook.com/659ba2ba1ffd3734c48d6498/playlist.m3u8'})
 
+if resp.status_code == 200:
+    headers = {
+        'Host': 'https://app.magmail.eu.org/get_keys',
+        'user-agent': 'Mobile-Android',
+        'device-id': 'c28d3cb16bbdac01',
+        'device-details': 'Xiaomi_Redmi 7_SDK-32',
+        'accept-encoding': 'gzip, deflate, br',
+    }
 
+    params = (('url', f'{url}'), )
 
-
-headers = {
-                    'Host': 'https://app.magmail.eu.org/get_keys',
-                    
-                    'user-agent': 'Mobile-Android',
-                    
-                    'device-id': 'c28d3cb16bbdac01',
-                    'device-details':
-                    'Xiaomi_Redmi 7_SDK-32',
-                    'accept-encoding': 'gzip, deflate, br',
-                    
-                    
-                }
-
-                params = (('url', f'{url}'), )
-
-                response = requests.get(
-                    'https://app.magmail.eu.org/get_keys',
-                    headers=headers,
-                    params=params)
-                # print(response.json())
-                url1 = response.json()['url']
-            else:
-                url1 = url
-              print(resp.text)
+    response = s.get('https://app.magmail.eu.org/get_keys', headers=headers, params=params)
+    
+    if response.status_code == 200:
+        url1 = response.json()['url']
+    else:
+        url1 = url
+        
+    print(resp.text)
+else:
+    print("Failed to get response:", resp.status_code)
