afd02e59ab3a47150567bfe7e3d095e8781f0c55
pandurajaking <133520032+pandurajaking@users.noreply.github.com>
2024-05-18 12:29:20 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,6 +3,4 @@ import requests as s
 url = 'https://cpvod.testbook.com/659ba2ba1ffd3734c48d6498/playlist.m3u8'
 
 resp = s.get('https://app.magmail.eu.org/get_keys', json={'link': url})
-
- 
-    print(resp.text)
+print(resp.text)
