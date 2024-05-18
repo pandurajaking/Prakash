5846dc72af675684eeafcc782a47ab56247dd4f5
pandurajaking <133520032+pandurajaking@users.noreply.github.com>
2024-05-18 11:23:02 +0530

    Update vcipher_keys.py

--- vcipher_keys.py
+++ vcipher_keys.py
@@ -1,6 +1,6 @@
 import requests
 
-# Replace this with your token (Smaller One)
+# Replace this with your token (Smaller One)#
 token = "eyJjb250ZW50QXV0aCI6ImV5SmpiMjUwWlc1MFNXUWlPaUk1WTJFM1pXUmhZems1WlRGa01HWTNNbVJpWkRJMVpXUTBOVE00WVRZd015SXNJbVY0Y0dseVpYTWlPaUl4TmprME56RTJOVEk0SWl3aWJHbGpaVzV6WlZKMWJHVnpJam9pZTF3aVkyRnVVR1Z5YzJsemRGd2lPbVpoYkhObExGd2ljbVZ1ZEdGc1JIVnlZWFJwYjI1Y0lqb3hPREF3TUgwaWZRPT0iLCJzaWduYXR1cmUiOiI3ZTU0NjliOGRmMDQ0ZmFjOjIwMjMwOTE0VDE4MzQ1OFo6ZHEwdndNSTl5eGZHLVdzeHk1MTdYS1BDdi10UnF5ZlltUVpvc1gwcGQyTT0ifQ=="
 response = requests.post(
     "https://learnyst.devsrajput.com/free", 
