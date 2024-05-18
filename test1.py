8e19cf3e710ac48e20c1607e8fabfc8b58054ba9
pandurajaking <133520032+pandurajaking@users.noreply.github.com>
2024-05-18 10:19:23 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,11 +1,7 @@
-import requests as s
+import cloudscraper
 
-session = s.Session()
-session.headers.update({
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-})
-
-resp = session.post(
+scraper = cloudscraper.create_scraper()
+resp = scraper.post(
     'https://app.magmail.eu.org/get_keys',
     json={'link': 'https://cpvod.testbook.com/659ba2ba1ffd3734c48d6498/playlist.m3u8'}
 )
