26e35b28cd94ce4426360c91dbac59218e008857
pandurajaking <133520032+pandurajaking@users.noreply.github.com>
2024-05-18 11:46:46 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@ import requests as s
 
 resp = s.post('https://app.magmail.eu.org/get_keys', json={'link':'https://cpvod.testbook.com/659ba2ba1ffd3734c48d6498/playlist.m3u8'})
 
-print(resp.text)
+
 
 
 headers = {
@@ -28,3 +28,4 @@ headers = {
                 url1 = response.json()['url']
             else:
                 url1 = url
+              print(resp.text)
