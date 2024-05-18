c0d1f61f0bf6052f91f471ba1d5ef713ea562e6f
pandurajaking <133520032+pandurajaking@users.noreply.github.com>
2024-05-18 11:13:04 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,9 +1,23 @@
-import cloudscraper
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
 
-scraper = cloudscraper.create_scraper()
-resp = scraper.post(
-    'https://app.magmail.eu.org/get_keys',
-    json={'link': 'https://cpvod.testbook.com/659ba2ba1ffd3734c48d6498/playlist.m3u8'}
-)
+options = Options()
+options.headless = True  # Run Chrome in headless mode
+options.add_argument("--disable-gpu")  # Disables GPU hardware acceleration
+options.add_argument("--no-sandbox")  # Required for running on some systems
+options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
 
-print(resp.text)
+driver = webdriver.Chrome(options=options)
+
+try:
+    driver.get('https://app.magmail.eu.org/get_keys')
+    time.sleep(5)  # Adjust the sleep time if necessary to allow the page to load
+    input_element = driver.find_element_by_name('link')
+    input_element.send_keys('https://cpvod.testbook.com/659ba2ba1ffd3734c48d6498/playlist.m3u8')
+    input_element.submit()
+    time.sleep(5)  # Wait for the form submission to complete
+    response = driver.page_source
+    print(response)
+finally:
+    driver.quit()
