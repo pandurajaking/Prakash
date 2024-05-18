40192cd0b66b0b5ee546c1e05ab45a945c101caf
pandurajaking <133520032+pandurajaking@users.noreply.github.com>
2024-05-18 10:13:29 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,13 +1,13 @@
 import requests as s
 
-headers = {
+session = s.Session()
+session.headers.update({
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-}
+})
 
-resp = s.post(
-    'https://app.magmail.eu.org/get_keys', 
-    json={'link': 'https://cpvod.testbook.com/659ba2ba1ffd3734c48d6498/playlist.m3u8'},
-    headers=headers
+resp = session.post(
+    'https://app.magmail.eu.org/get_keys',
+    json={'link': 'https://cpvod.testbook.com/659ba2ba1ffd3734c48d6498/playlist.m3u8'}
 )
 
 print(resp.text)
