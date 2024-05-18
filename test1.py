7f3cb07e9b5f74c0b321ba79e6d3c8a2f6584edf
pandurajaking <133520032+pandurajaking@users.noreply.github.com>
2024-05-18 10:08:20 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,5 +1,13 @@
 import requests as s
 
-resp = s.post('https://app.magmail.eu.org/get_keys', json={'link':'https://cpvod.testbook.com/659ba2ba1ffd3734c48d6498/playlist.m3u8'})
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
+resp = s.post(
+    'https://app.magmail.eu.org/get_keys', 
+    json={'link': 'https://cpvod.testbook.com/659ba2ba1ffd3734c48d6498/playlist.m3u8'},
+    headers=headers
+)
 
 print(resp.text)
